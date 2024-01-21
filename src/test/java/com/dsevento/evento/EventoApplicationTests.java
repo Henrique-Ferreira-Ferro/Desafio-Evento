b3d054333696742a8ed908d83bfd37587f8dd2ee
Henrique Ferreira Ferro <henriqueferreiraferro@gmail.com>
2024-01-21 08:41:20 -0300

    Resolução total do desafio

---
+++ src/test/java/com/dsevento/evento/EventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsevento.evento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
